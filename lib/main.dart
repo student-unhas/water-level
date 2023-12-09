8a1e0a2df7929882a1b987c4b586d5cc239798ad
AsdarMLD <53163040+asdardevs@users.noreply.github.com>
2023-12-09 17:16:44 +0800

    update data

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:final_cimming/firebase_options.dart';
 import 'package:final_cimming/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessaging.instance.subscribeToTopic("user");
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+
+  print(fcmToken);
   runApp(const MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,12 @@ import Foundation
 
 import firebase_core
 import firebase_database
+import firebase_messaging
+import flutter_local_notifications
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
 }
